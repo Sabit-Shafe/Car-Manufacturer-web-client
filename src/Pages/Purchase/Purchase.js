ffe47d7b322f2a4a7b1896de5be03e1925316d53
Sabitshafeexit(0clear <sabitshaafee@gmail.com>
2022-05-26 16:51:08 +0600

    Purchase page change

--- src/Pages/Purchase/Purchase.js
+++ src/Pages/Purchase/Purchase.js
@@ -1,23 +1,22 @@
-import React, { useState } from 'react';
+import React, { } from 'react';
 import { useForm } from 'react-hook-form';
 import { Link, useNavigate, useParams } from 'react-router-dom';
 import { toast } from 'react-toastify';
 import useProductDetail from '../../hooks/useProductDetail';
-import { useAuthState } from 'react-firebase-hooks/auth';
-import auth from '../../firebase.init';
+
 
 const Purchase = () => {
     const navigate = useNavigate();
     const navigateToItemDetail = id => {
-      navigate(`/dashboard/MyOrder/${id}`);
+        navigate(`/dashboard/MyOrder/${id}`);
     }
     const { productsID } = useParams();
     const [products] = useProductDetail(productsID)
-    
+
     const { register, formState: { errors }, handleSubmit, reset } = useForm();
-    
+
     const onSubmit = (order) => {
-        
+
         console.log(order);
         fetch('http://localhost:5000/order', {
             method: 'POST',
@@ -48,100 +47,98 @@ const Purchase = () => {
                     <h2 class="card-title">{products.name}</h2>
                     <p> Product Description: {products.description}</p>
                     <h3 className="text-lg font-bold text-fuchsia-500">price per unit: {products.price}<span> $</span></h3>
-                 </div>
-           </div>
-           <div className=" mx-5">
-            <form onSubmit={handleSubmit(onSubmit)} className="">
-            <div className="form-control w-full max-w-xs">
-                    <label className="label">
-                        <span className="label-text">{products.name}</span>
-                    </label>
-                    <input
-                        // disabled value={products?.name || ''}
-                        type="text"
-                        placeholder=" Product Name"
-                        className="input input-bordered w-full max-w-xs"
-                        {...register("ProductName")}
-                    />
-                    <label className="label">
-                        {errors.name?.type === 'required' && <span className="label-text-alt text-red-500">{errors.name.message}</span>}
-                    </label>
                 </div>
-
-                <div className="form-control w-full max-w-xs">
-                    <label className="label">
-                        <span className="label-text">Name</span>
-                    </label>
-                    <input
-                        // disabled value={user?.displayName || ''}
-                        type="text"
-                        placeholder="Name"
-                        className="input input-bordered w-full max-w-xs"
-                        {...register("name")}
-                    />
-                    <label className="label">
-                        {errors.name?.type === 'required' && <span className="label-text-alt text-red-500">{errors.name.message}</span>}
-                    </label>
-                </div>
-                <div className="form-control w-full max-w-xs">
-                    <label className="label">
-                        <span className="label-text">Email</span>
-                    </label>
-                    <input
-                        // disabled value={user?.email || ''}
-                        type="email"
-                        placeholder="Your Email"
-                        className="input input-bordered w-full max-w-xs"
-                        {...register("email")}
-                    />
-                    <label className="label">
-                        {errors.email?.type === 'required' && <span className="label-text-alt text-red-500">{errors.email.message}</span>}
-                        {errors.email?.type === 'pattern' && <span className="label-text-alt text-red-500">{errors.email.message}</span>}
-                    </label>
-                </div>
-
-                <div className="form-control w-full max-w-xs">
-                    <label className="label">
-                        <span className="label-text">Address</span>
-                    </label>
-                    <input
-                        type="text"
-                        placeholder="Address"
-                        className="input input-bordered w-full max-w-xs"
-                        {...register("address", {
-                            required: {
-                                value: true,
-                                message: 'Address is Required'
-                            },
-
-                        })}
-                    />
-                </div>
-                <div className="form-control w-full max-w-xs">
-                    <label className="label">
-                        <span className="label-text">Quantity</span>
-                    </label>
-                    <input className='mb-2 input input-bordered w-full max-w-xs' 
-                    placeholder='Quantity' type="number" {...register("quantity")} />
-                </div>
-
-                <div className="form-control w-full max-w-xs">
-                    <label className="label">
-                        <span className="label-text">Price</span>
-                    </label>
-                    <input className='mb-2 input input-bordered w-full max-w-xs' placeholder='price' type="number" {...register("price")} />
-                </div>
-
-
-                <input className='btn w-full max-w-xs text-white' type="submit" value="Purchase" />
-            </form>
-            <div className="text-center mb-5">
-            <Link to={`/dashboard/MyOrder`}><button className='btn btn-xl btn-success mt-5 mb-5'>Order Payment</button></Link>
             </div>
+            <div className=" mx-5">
+                <form onSubmit={handleSubmit(onSubmit)} className="">
+                    <div className="form-control w-full max-w-xs">
+                        <label className="label">
+                            <span className="label-text">{products.name}</span>
+                        </label>
+                        <input
+                            // disabled value={products?.name || ''}
+                            type="text"
+                            placeholder=" Product Name"
+                            className="input input-bordered w-full max-w-xs"
+                            {...register("ProductName")}
+                        />
+                        <label className="label">
+                            {errors.name?.type === 'required' && <span className="label-text-alt text-red-500">{errors.name.message}</span>}
+                        </label>
+                    </div>
+
+                    <div className="form-control w-full max-w-xs">
+                        <label className="label">
+                            <span className="label-text">Name</span>
+                        </label>
+                        <input
+                            // disabled value={user?.displayName || ''}
+                            type="text"
+                            placeholder="Name"
+                            className="input input-bordered w-full max-w-xs"
+                            {...register("name")}
+                        />
+                        <label className="label">
+                            {errors.name?.type === 'required' && <span className="label-text-alt text-red-500">{errors.name.message}</span>}
+                        </label>
+                    </div>
+                    <div className="form-control w-full max-w-xs">
+                        <label className="label">
+                            <span className="label-text">Email</span>
+                        </label>
+                        <input
+                            // disabled value={user?.email || ''}
+                            type="email"
+                            placeholder="Your Email"
+                            className="input input-bordered w-full max-w-xs"
+                            {...register("email")}
+                        />
+                        <label className="label">
+                            {errors.email?.type === 'required' && <span className="label-text-alt text-red-500">{errors.email.message}</span>}
+                            {errors.email?.type === 'pattern' && <span className="label-text-alt text-red-500">{errors.email.message}</span>}
+                        </label>
+                    </div>
+
+                    <div className="form-control w-full max-w-xs">
+                        <label className="label">
+                            <span className="label-text">Address</span>
+                        </label>
+                        <input
+                            type="text"
+                            placeholder="Address"
+                            className="input input-bordered w-full max-w-xs"
+                            {...register("address", {
+                                required: {
+                                    value: true,
+                                    message: 'Address is Required'
+                                },
+
+                            })}
+                        />
+                    </div>
+                    <div className="form-control w-full max-w-xs">
+                        <label className="label">
+                            <span className="label-text">Quantity</span>
+                        </label>
+                        <input className='mb-2 input input-bordered w-full max-w-xs'
+                            placeholder='Quantity' type="number" {...register("quantity")} />
+                    </div>
+
+                    <div className="form-control w-full max-w-xs">
+                        <label className="label">
+                            <span className="label-text">Price</span>
+                        </label>
+                        <input className='mb-2 input input-bordered w-full max-w-xs' placeholder='price' type="number" {...register("price")} />
+                    </div>
+                    <input className='btn w-full max-w-xs text-white' type="submit" value="Purchase" />
+                </form>
+                <div className="text-center mb-5">
+                    <Link to={`/dashboard/MyOrder`}><button className='btn btn-xl btn-success mt-5 mb-5'>Order Payment</button></Link>
+                </div>
             </div>
-           
+
         </div>
-       
+
     );
 };
 
