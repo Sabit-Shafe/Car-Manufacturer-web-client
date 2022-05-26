6a22ee86f13bd8401f3c0116bfbfac4f3844642a
Sabitshafeexit(0clear <sabitshaafee@gmail.com>
2022-05-26 16:51:50 +0600

    Purchase page change

--- src/Pages/Shared/Footer.js
+++ src/Pages/Shared/Footer.js
@@ -19,7 +19,7 @@ const Footer = () => {
                     </div>
                 </div>
                 <div>
-                    <p>Copyright © 2022 - All right reserved by ACME Industries Ltd</p>
+                    <p>Copyright © 2022 - All right reserved by Car Mania Industries Ltd</p>
                 </div>
             </footer>
         </div>
